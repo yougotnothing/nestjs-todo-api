8407aaa871aa34232104488c6aca21a2e13cafcb
влад троян <loool.ke.k.6.6.6@gmail.com>
2024-05-25 01:43:35 +0200

    feature: Add getTasksLength request. Integrated .env into dbconfig. Refactoring.

---
+++ dbconfig.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const ormconfig = (configService: ConfigService): TypeOrmModuleOptions => ({
+  type: "postgres",
+  host: "localhost",
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  synchronize: true,
+  logging: true,
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  migrations: ["dist/migrations/*{.ts,.js}"],
+});
