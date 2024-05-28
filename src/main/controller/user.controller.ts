801fee2ce3ed80667d68705a84b103aab1ede0f3
влад троян <loool.ke.k.6.6.6@gmail.com>
2024-05-28 06:32:31 +0200

    fix: Refactoring.

--- src/main/controller/user.controller.ts
+++ src/main/controller/user.controller.ts
@@ -7,7 +7,6 @@ import {
   HttpException,
   HttpStatus,
   Req,
-  Delete,
   HttpCode,
   Query,
   UploadedFile,
@@ -15,11 +14,7 @@ import {
   Header,
   Res
 } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { UserEntity } from "entity/user.entity";
 import { UserService } from "service/user.service";
-import { CreateTodoDto } from "types/create.todo.dto";
 import { Auth } from "guard/auth.guard";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Response } from "express";
@@ -28,8 +23,6 @@ import * as multer from "multer";
 @Controller('user')
 export class UserController {
   constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
     private readonly userService: UserService,
     private readonly auth: Auth
   ) {}
@@ -39,10 +32,7 @@ export class UserController {
     storage: multer.memoryStorage()
   }))
   @HttpCode(200)
-  @Header('Content-Type', 'multipart/form-data')
   async changeAvatar(@UploadedFile() avatar: Express.Multer.File, @Req() req: Request) {
-    console.log('file received: ', avatar);
-
     if(!avatar) throw new HttpException("file is empty.", HttpStatus.BAD_REQUEST);
 
     const validation = await this.auth.validate(req.headers['authorization'].split(' ')[1]);
@@ -54,29 +44,12 @@ export class UserController {
 
   @Patch('/change-name')
   @HttpCode(200)
-  async changeName(@Body() body: { id: number, name: string }) {
-    return await this.userService.changeName(body.id, body.name);
-  }
-
-  @Post('/add-task')
-  @HttpCode(200)
-  async addTask(@Body() task: CreateTodoDto, @Req() req: Request) {
+  async changeName(@Body() name: string, @Query('id') id: number, @Req() req: Request) {
     const validation = await this.auth.validate(req.headers['authorization'].split(' ')[1]);
 
     if(!validation.isValid) throw new HttpException("token is invalid.", HttpStatus.UNAUTHORIZED);
 
-    return await this.userService.addTask(task, validation.name);
-  }
-
-  @Delete('/delete-task')
-  @HttpCode(200)
-  async deleteTask(@Req() req: Request, @Body() task_id: number) {
-    const validation = await this.auth.validate(req.headers['authorization'].split(' ')[1]);
-    const user = await this.userRepository.findOneBy({ name: validation.name });
-
-    if(!validation.isValid) throw new HttpException("token is invalid.", HttpStatus.UNAUTHORIZED);
-
-    return await this.userService.deleteTask(user.id, task_id);
+    return await this.userService.changeName(id, name);
   }
 
   @Get('/get-tasks')
--- src/main/service/user.service.ts
+++ src/main/service/user.service.ts
@@ -4,7 +4,6 @@ import { Repository } from "typeorm";
 import { UserEntity } from "entity/user.entity";
 import { TodoEntity } from "entity/todo.entity";
 import { PublicUserDto } from "types/public.user.dto";
-import { CreateTodoDto } from "types/create.todo.dto";
 import { Auth } from "guard/auth.guard";
 
 @Injectable()
@@ -14,7 +13,6 @@ export class UserService {
     private readonly todoRepository: Repository<TodoEntity>,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-    private readonly auth: Auth
   ) {}
 
   async changeAvatar(avatar: Buffer, name: string): Promise<{ message: string }> {
@@ -22,23 +20,14 @@ export class UserService {
 
     if(!user) throw new HttpException("User not found", HttpStatus.NOT_FOUND);
 
-    console.log("User found:", user);
-
     user.changeAvatar(avatar);
 
-    console.log("User before save:", user);
-
     await this.userRepository.save(user);
-
-    const updatedUser = await this.userRepository.findOneBy({ name });
-    console.log("User after save:", updatedUser);
 
     return {
       message: "Avatar changed."
     };
   }
-
-
 
   async changeName(id: number, name: string): Promise<{ message: string }> {
     const user = await this.userRepository.findOneBy({ id });
@@ -53,57 +42,14 @@ export class UserService {
   async getTasks(username: string): Promise<{ message: string, tasks: TodoEntity[] }> {
     const tasks = await this.todoRepository.findBy({ creator: username });
 
-    console.log('tasks: ', tasks);
-
-    if(!tasks.length) {
-      return {
-        message: "user has no tasks.",
-        tasks: []
-      }
+    if(!tasks.length) return {
+      message: "user has no tasks.",
+      tasks: []
     }
 
     return {
       message: "user tasks.",
       tasks: tasks
-    }
-  }
-
-  async addTask(task: CreateTodoDto, username: string): Promise<{ message: string }> {
-    const user = await this.userRepository.findOneBy({ name: username });
-    const newTask = new TodoEntity();
-
-    if(!task.header.length) throw new HttpException("header cannot be empty.", HttpStatus.BAD_REQUEST);
-    
-    newTask.header = task.header;
-    newTask.creator = username;
-    newTask.user = user;
-    newTask.isChecked = task.isChecked;
-    newTask.important = task.important;
-    newTask.createdAt = task.createdAt;
-    newTask.till = task.till;
-    newTask.from = task.from;
-    newTask.type = task.type;
-    newTask.tasks = task.tasks;
-    
-    await this.todoRepository.save(newTask);
-    
-    return {
-      message: "task added."
-    }
-  }
-
-  async deleteTask(id: number, taskId: number): Promise<{ message: string }> {
-    const user = await this.userRepository.findOneBy({ id });
-    const task = await this.todoRepository.findOneBy({ id: taskId });
-
-    if(!user.tasks.includes(task)) return { message: "task not found." }
-
-    await this.todoRepository.delete(task);
-    await this.todoRepository.save(task);
-    await this.userRepository.save(user);
-
-    return {
-      message: "task deleted."
     }
   }
 
@@ -118,7 +64,8 @@ export class UserService {
         isHaveAvatar: user.isHaveAvatar,
         name: user.name,
         email: user.email,
-        avatar: `${process.env.API_URL}/user/get-avatar?id=${user.id}`
+        avatar: `${process.env.API_URL}/user/get-avatar?id=${user.id}`,
+        id: user.id
       }
     }
   }
