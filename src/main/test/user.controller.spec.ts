f0feb2d5299231a6ea5da392c104452621180491
влад троян <loool.ke.k.6.6.6@gmail.com>
2024-05-30 14:43:19 +0200

    feature: add unit test to user controller. Create new branch 'request'.

---
+++ src/main/test/user.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { UserController } from "controller/user.controller";
+import { UserService } from "service/user.service";
+
+describe("UserController", () => {
+  let controller: UserController;
+
+  beforeEach(async () => {
+    const userModule: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService]
+    }).compile();
+
+    controller = userModule.get<UserController>(UserController);
+
+    describe("get-user", () => {
+      it("should return a user", async () => {
+        expect(await controller.getUser(1)).toBeDefined();
+      });
+    });
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+});
